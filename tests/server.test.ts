6c146d8c9816cc5a537d339cb3d43a6ec1643494
Ryota Saiga <>
2023-02-20 22:13:41 +0900

    add test

---
+++ tests/server.test.ts
@@ -0,0 +1,17 @@
+import request from 'supertest'
+import { app } from '../src/app'
+
+describe(`Hello world`, () => {
+  test(`GET`, (done) => {
+    request(app)
+      .get(`/`)
+      .then((res) => {
+        expect(res.statusCode).toBe(200)
+        expect(res.text).toBe(`Hello World!`)
+        done()
+      })
+      .catch((err) => {
+        done(err)
+      })
+  })
+})
