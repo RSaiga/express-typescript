bfeb88584ed1a527942276b5cc944c7494662fd3
Ryota Saiga <>
2023-02-22 08:50:08 +0900

    refactor format

--- src/controllers/body_records/search_body_records_controller.ts
+++ src/controllers/body_records/search_body_records_controller.ts
@@ -1,20 +1,19 @@
-import {Controller, Get, Route, Tags} from 'tsoa'
-import {BodyRecord} from "../../domain/models/body_records/body_record";
+import { Controller, Get, Route, Tags } from 'tsoa'
+import { type BodyRecord } from '../../domain/models/body_records/body_record'
 
 @Route('body_records')
 @Tags('body_records')
 export class SearchBodyRecordsController extends Controller {
   @Get()
-  public async get (): Promise<BodyRecord[]> {
+  public async get(): Promise<BodyRecord[]> {
     return [
       { id: 'test2', weight: 64, fat_rate: 20.1, date: '2023-02-03' },
       {
         id: 'test1',
         weight: 65,
         fat_rate: 20.8,
-        date: '2023-02-02'
-      }
+        date: '2023-02-02',
+      },
     ]
   }
 }
-
