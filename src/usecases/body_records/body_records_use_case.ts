96b41187d5e05fd5262c3d130eccbc3922a080d5
Ryota Saiga <>
2023-02-22 09:15:23 +0900

    refactor repository layer

--- src/usecases/body_records/body_records_use_case.ts
+++ src/usecases/body_records/body_records_use_case.ts
@@ -1,8 +1,6 @@
 import { type BodyRecord } from '../../domain/models/body_records/body_record'
+import { BodyRecordsRepository } from '../../domain/repositories/body_records/body_records_repository'
 
 export class BodyRecordsUseCase {
-  find = (): BodyRecord[] => [
-    { id: 'test2', weight: 64, fat_rate: 20.1, date: '2023-02-03' },
-    { id: 'test1', weight: 65, fat_rate: 20.8, date: '2023-02-02' },
-  ]
+  find = (): BodyRecord[] => new BodyRecordsRepository().find()
 }
