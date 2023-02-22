c9923eb5ad2a004777b7976bb3878b612f633a17
Ryota Saiga <>
2023-02-22 09:10:51 +0900

    refactor move usecase

--- src/controllers/body_records/search_body_records_controller.ts
+++ src/controllers/body_records/search_body_records_controller.ts
@@ -1,12 +1,6 @@
 import { Controller, Get, Route, Tags } from 'tsoa'
 import { type BodyRecord } from '../../domain/models/body_records/body_record'
-
-class BodyRecordsUseCase {
-  find = (): BodyRecord[] => [
-    { id: 'test2', weight: 64, fat_rate: 20.1, date: '2023-02-03' },
-    { id: 'test1', weight: 65, fat_rate: 20.8, date: '2023-02-02' },
-  ]
-}
+import { BodyRecordsUseCase } from '../../usecases/body_records/body_records_use_case'
 
 @Route('body_records')
 @Tags('body_records')
---
+++ src/usecases/body_records/body_records_use_case.ts
@@ -0,0 +1,8 @@
+import { type BodyRecord } from '../../domain/models/body_records/body_record'
+
+export class BodyRecordsUseCase {
+  find = (): BodyRecord[] => [
+    { id: 'test2', weight: 64, fat_rate: 20.1, date: '2023-02-03' },
+    { id: 'test1', weight: 65, fat_rate: 20.8, date: '2023-02-02' },
+  ]
+}
