5a65f5e5089a406f9be6a2ed3dd0103094046350
Ryota Saiga <>
2023-02-22 09:09:11 +0900

    refactor usecase layer

--- src/controllers/body_records/search_body_records_controller.ts
+++ src/controllers/body_records/search_body_records_controller.ts
@@ -1,19 +1,18 @@
 import { Controller, Get, Route, Tags } from 'tsoa'
 import { type BodyRecord } from '../../domain/models/body_records/body_record'
 
+class BodyRecordsUseCase {
+  find = (): BodyRecord[] => [
+    { id: 'test2', weight: 64, fat_rate: 20.1, date: '2023-02-03' },
+    { id: 'test1', weight: 65, fat_rate: 20.8, date: '2023-02-02' },
+  ]
+}
+
 @Route('body_records')
 @Tags('body_records')
 export class SearchBodyRecordsController extends Controller {
   @Get()
   public async get(): Promise<BodyRecord[]> {
-    return [
-      { id: 'test2', weight: 64, fat_rate: 20.1, date: '2023-02-03' },
-      {
-        id: 'test1',
-        weight: 65,
-        fat_rate: 20.8,
-        date: '2023-02-02',
-      },
-    ]
+    return new BodyRecordsUseCase().find()
   }
 }
