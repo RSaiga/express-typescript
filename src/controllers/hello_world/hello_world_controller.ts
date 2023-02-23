62bbe342112f20b55742df27159c2ca5886afa67
Ryota Saiga <>
2023-02-23 14:30:11 +0900

    delete hello world

--- src/controllers/hello_world/hello_world_controller.ts
+++
@@ -1,5 +0,0 @@
-import { type Request, type Response } from 'express'
-
-export const HelloWorldController = (req: Request, res: Response): Response => {
-  return res.send('Hello World!')
-}
--- src/routes/hello_world/hello_world_router.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from 'express'
-import { HelloWorldController } from '../../controllers/hello_world/hello_world_controller'
-
-export const HelloWorldRouter = Router()
-
-HelloWorldRouter.get('/', HelloWorldController)
